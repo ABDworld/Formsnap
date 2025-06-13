1644bd098a26253404327f2bcc6b4319732a3dcc
ABDworld <abdecom2024@gmail.com>
2025-06-13 23:10:46 +0200

    Use FormSnapBuilder in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,12 @@
-console.log("✅ App.jsx is being loaded");
-
 import React from "react";
-import PaymentButton from "./components/PaymentButton";
+import FormSnapBuilder from "./components/FormSnapBuilder";
 
 function App() {
   return (
-    <div style={{ padding: "40px", fontFamily: "sans-serif" }}>
-      <h1>FormSnap</h1>
-
-      {/* 🔵 Stripe Payment Button */}
-      <PaymentButton />
-
-      {/* 🔵 Example Form */}
-      <form style={{ marginTop: "20px" }}>
-        <div style={{ marginBottom: "10px" }}>
-          <label>
-            Your name:
-            <input type="text" name="name" style={{ marginLeft: "10px" }} />
-          </label>
-        </div>
-        <button type="submit">Submit</button>
-      </form>
+    <div>
+      <FormSnapBuilder />
     </div>
   );
 }
 
 export default App;
-// Vercel redeploy attempt
