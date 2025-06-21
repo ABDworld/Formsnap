5a02c02d7c4d14dcf926a96865096ebc1982bcd7
ABDworld <abdecom2024@gmail.com>
2025-06-21 22:27:05 +0200

    🛠 Add Next.js config to enable API routes

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // 🔧 Très important si tes pages sont dans /src/pages
-  experimental: {
-    appDir: false,
-  },
 };
 
 module.exports = nextConfig;
